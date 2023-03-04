8385c12455c7c9ea8e084227a6456e3dc037820c
Phoenix Lai <plai40@my.bcit.ca>
2023-03-04 11:31:23 -0800

    add filees

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+5. The application must allow the user to store notes in a database and retrieve them
+6. The application must allow the user to store images in a database (not in the filesystem) and retrieve them
+7. Users must be able to update notes by editing a textarea within a form
+8. Users must be able to upload a maximum of four images, and delete images
+9. The system must provide the following access controls:
+a. Anyone who is not logged in, and who attempts to view any page besides the login / register page(s)
+will be redirected to the register page.
+b. A logout function is provided - phoenix
+c. Users who are inactive for more than 10 minutes are logged out - phoenix
+*/
+
+// notes READ and DELETE, display a notes list, and allow the user to delete notes that they created
+// if the user is not logged in, redirect to the login page - phoenix
+
+?>
---
+++ recieve.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+1. The system must have membership-signup functionality including:
+a. No duplicate usernames are permitted
+b. Username must be an email address
+c. Passwords are hashed when stored (i.e., the database does not contain the actual password)
+2. There must be a “reset password” function whereby users who forget their passwords can get an email
+with a new password in it. Your application must generate a new password and update the user’s
+database record with it.
+3. After registering, an email is sent to the user with a hyperlink in it. When the user clicks the link, the
+account becomes “active” and users can log in. Users cannot login to the system until this link has been
+clicked.
+4. If a user fails three times in a row to give the correct password for a given username, the account
+becomes “locked” and an email is sent to the user
+*/
+
+// receive the login data and validate
+// if valid, redirect to the notes list page?
+
+?>
