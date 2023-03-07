918de6c599af776fb4ee3ed99a58f66ef56963cd
Phoenix Lai <plai40@my.bcit.ca>
2023-03-06 17:44:03 -0800

    almost done login stuff

--- activateUser.php
+++ activateUser.php
@@ -3,4 +3,31 @@
 // find the user in the db, and set the active field to true
 // redirect to login page
 
-?>
+$servername = "containers-us-west-119.railway.app";
+$username = "root";
+$password = "yo5sZZTb4tea7SMAl4s2";
+$database = "railway";
+$port = 7628;
+
+try {
+    $mysql = new mysqli($servername, $username, $password, $database, $port);
+    if ($mysql === false) {
+        die("ERROR: Could not connect. "
+            . mysqli_connect_error());
+    } else {
+        echo "Connected to database successfully";
+    }
+
+    $url = $_SERVER['REQUEST_URI'];
+    $url_components = parse_url($url);
+    parse_str($url_components['query'], $params);
+
+    $email = $params['email'];
+
+    $sql = "UPDATE `user` SET `active` = 1 WHERE `email` = '$email'";
+    $mysql->query($sql);
+    header("location: login.php");
+
+} catch (mysqli_sql_exception $e) {
+    throw $e;
+}
